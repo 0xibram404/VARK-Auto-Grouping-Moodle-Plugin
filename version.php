e433dd64f397ba44d8beebb9246f60f8a5e72150
0xibram404 <ibrahim.mahmoud@sed.svu.edu.eg>
2026-04-07 15:18:50 +0200

    update to version 1.2.0

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->component = 'local_varkassign'; // الاسم البرمجي للإضا
 $plugin->version   = 2026040700;         // إصدار الإضافة (سنة-شهر-يوم-رقم)
 $plugin->requires  = 2019111800;         // أقل إصدار مطلوب من مودل (3.9 مثلاً)
 $plugin->maturity  = MATURITY_STABLE;    // حالة الإضافة (مستقرة)
-$plugin->release   = '1.1.0';            // رقم الإصدار الظاهر للمستخدم
+$plugin->release   = '1.2.0';            // رقم الإصدار الظاهر للمستخدم
